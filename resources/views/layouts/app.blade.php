b5622f718387c8a27302e7a9a80d00db6edcce0a
bevan tony medrano <babantoto@gmail.com>
2018-12-09 13:09:13 +0800

    tutorial #4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
         <title>{{config('app.name', 'LSAPP')}}</title>
     </head>
     <body>
+        @include('inc.navbar');
+        <div class="container">
         @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 
-
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is the Laravel application from the "Laravel from Scratch" Youtube Series</p>
+
+        <div class="jumbotron text-center">
+          <h1>Welcome to Laravel</h1>
+          <p>This is the laravel application from the "laravel from scratch" Youtube series</p>
+          <p><a class="btn btn-primary btn-lg" href="/login" role="button"> Login </a> <a class="btn btn-success btn-lg" href="/register" role="button">Register </a> </p> 
+          </div>
+
+
 @endsection
         
 
